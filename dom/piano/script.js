4668fcc4e53c79385770b30f1897d05197cef499
Saad Desktop <saadsohailbajwa@gmail.com>
2023-04-27 04:49:12 +0500

    started working on piano project
    made piano folder within dom folder

---
+++ dom/piano/script.js
@@ -0,0 +1,10 @@
+for(var i=0; i<document.querySelectorAll("button").length; i++){
+    document.querySelectorAll("button")[i].addEventListener("click", function(i){
+        
+        alert("hello")
+        }
+    )
+
+    
+
+}
