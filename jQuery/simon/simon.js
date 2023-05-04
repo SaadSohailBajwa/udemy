1a82eddde4ebe60edad77e70de60d7e9fe474be6
Saad Desktop <saadsohailbajwa@gmail.com>
2023-05-05 04:40:10 +0500

    added new features to the simon game and did some changes

--- jQuery/simon/simon.js
+++ jQuery/simon/simon.js
@@ -1,4 +1,75 @@
-$(".yellow").on("click",function(){
+let levels = []
+let playerLevel = []
+let globalCounter = 0
+
+
+console.log(levels)
+
     
-    $(".yellow").addClass("border")
+console.log(document.querySelector(".b-1").classList)    
+
+
+function assignEventListener(x){
+    $(".b-"+x).on("click",function(){
+    
+    $(".b-"+x).addClass("border-click");
+    setTimeout(function(){
+        $(".b-"+x).removeClass("border-click")
+    },200);
+
+    
+
+
+    playerLevel.push(x)
+    if(playerLogic() == true){
+        levels.push(randomGen());
+        $("h1").text("you win");
+    }else{
+        $("h1").text("you lose");
+    }
+    console.log(playerLevel);
 });
+}
+
+for(let i=1;i<5;i++){
+    assignEventListener(i)
+}
+
+
+
+
+
+function randomGen(){
+    let nextLevel = Math.floor(Math.random()*4) + 1;
+    return nextLevel
+}
+
+
+
+function playerLogic(){
+
+    if(playerLevel[playerLevel.length-1] == levels[playerLevel.length-1]){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+function nextLevelHint(){
+
+    levels.push(randomGen())
+    setTimeout(function(){
+        $(".b-"+levels[levels.length-1]).addClass("border-next")
+    },2000);
+    setTimeout(function(){
+        $(".b-"+levels[levels.length-1]).removeClass("border-next")
+    },2500);
+
+}
+
+nextLevelHint()
+
+
+
+
+
