21e5efb72dc4b4388f9c0044061d9c0436e5d26f
Saad Desktop <saadsohailbajwa@gmail.com>
2023-05-12 03:08:09 +0500

    started working on backend
    made node folder installed packages using nmp install and then require to use in a file

---
+++ node/index.js
@@ -0,0 +1,7 @@
+const fs = require("fs")
+const superHeroes = require("superheroes")
+const superVillains = require("supervillains")
+
+
+console.log("name of the super hero is " + superHeroes.random())
+console.log("name of the super villain is "+ superVillains.random())
