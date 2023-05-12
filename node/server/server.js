1afaa4b327adc89a56fbfc558b9d23a466c2f520
Saad Desktop <saadsohailbajwa@gmail.com>
2023-05-12 05:24:00 +0500

    made express server
    learned about get method and req and res

---
+++ node/server/server.js
@@ -0,0 +1,17 @@
+//jshint esversion:6
+
+const express = require("express");
+
+const app = express();
+
+app.get("/",function(request,response){
+    response.send("<h1>hello</h1>")
+})
+
+app.get("/about" , (req,res)=>{
+    res.send("The owner of this page is Saad")
+})
+
+app.listen(3000, ()=>{
+    console.log("server started at port 3000")
+});
