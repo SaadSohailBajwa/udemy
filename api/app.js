48ce297bb63954bbbace4ef9bb38f61d954d8b24
Saad Desktop <saadsohailbajwa@gmail.com>
2023-05-22 02:21:40 +0500

    started working on API
    made weather api in which i learned how to send https.get req to weather api server and and respone.on function and also JSON.parse to convert hexa demical data into string

---
+++ api/app.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+const https = require("https");
+
+
+app.get("/",function(req,res){
+
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=Paris&units=metric&appid=0e1d121bd7344985fd816a717c929f52";
+    
+    https.get(url,function(response){
+
+        response.on("data",function(data){
+            const weatherData = JSON.parse(data)
+            console.log(weatherData)
+        })
+        
+    })
+
+    res.send("this is homepage")
+});
+
+app.get("/london",function(req,res){
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=London&units=metric&appid=0e1d121bd7344985fd816a717c929f52";
+
+    https.get(url,function(respone){
+        respone.on("data",function(data){
+            const London = JSON.parse(data);
+            console.log(London.name,London.timezone)
+            res.send("The timezone of london is: "+London.main.temp)
+        })
+    });
+
+    //res.send("this is london weather")
+})
+
+app.listen(3000,function(){
+    console.log("server started at port: 3000");
+});
