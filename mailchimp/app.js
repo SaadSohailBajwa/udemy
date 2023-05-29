5f12e537af6a1c8eca9bd32278cc90f7c19cd711
Saad Desktop <saadsohailbajwa@gmail.com>
2023-05-29 06:37:58 +0500

    completed api section of the course but mailChimp proj is not complete

--- mailchimp/app.js
+++ mailchimp/app.js
@@ -4,10 +4,19 @@ const port = 3000;
 const request = require("request");
 const bodyParser = require("body-parser");
 
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}))
 
+app.get('/',function(req,res){
+    res.sendFile(__dirname+"/sign-up.html")
+})
 
-
-
+app.post('/',function(req,res){
+    let fullName = req.body.inputName
+    let email = req.body.inputEmail
+    let password = req.body.inputPassword
+    console.log(fullName+" "+email+" "+password)
+})
 
 
 
@@ -15,3 +24,6 @@ const bodyParser = require("body-parser");
 app.listen(port,function(){
     console.log("server started at port: "+ port);
 });
+
+
+// 6108406fc695b652dd0eb1534ce37cfd-us21
